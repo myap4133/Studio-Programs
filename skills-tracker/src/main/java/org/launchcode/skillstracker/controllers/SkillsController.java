f8fc2deede784e7f010a215a7347a1f95f48e870
may2fn <may2fn@umsystem.edu>
2023-09-21 19:34:44 -0500

    Finished skill controller studio

--- skills-tracker/src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ skills-tracker/src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -14,6 +14,12 @@ public class SkillsController {
         return "<html>" +
                 "<body>" +
                 "<h1>Skills Tracker</h1>" +
+                "<h2>We have a few skills we would like to learn. Here is a list!</h2>" +
+                "<ol>" +
+                "<li>Java</li>" +
+                "<li>C++</li>" +
+                "<li>Python</li>" +
+                "</ol>" +
                 "<p>Click this <a href='/form'>form</a> to fill out your preferences.</p>" +
                 "</body>" +
                 "</html>";
@@ -23,25 +29,106 @@ public class SkillsController {
     public String skillForm(){
         return "<html>" +
                 "<body>" +
-                "<form action='/form' method='POST'>" +
-                "<input type='text' name='name'>" +
+                "<form action='/completed'>" + //"<form action='/form' method='POST'>" for first two original POST
+                "<b>Name:</b>" + "<br/>" +
+                "<input type='text' name='name'>" + "<br/>" +
+                "<b>My favorite language: </b>" + "<br/>" +
                 "<select name='first'>" +
-                "<option value='java'>Java</option>" +
-                "<option value='javascript'>JavaScript</option>" +
-                "<option value='python'>Python</option>" +
-                "</select>" +
-                "<input type='submit' value='Submit Information'>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='C++'>C++</option>" +
+                "<option value='Python'>Python</option>" +
+                "</select>" + "<br/>" +
+                "<b>My second favorite language: </b>" + "<br/>" +
+                "<select name='second'>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='C++'>C++</option>" +
+                "<option value='Python'>Python</option>" +
+                "</select>" + "<br/>" +
+                "<b>My third favorite language: </b>" + "<br/>" +
+                "<select name='third'>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='C++'>C++</option>" +
+                "<option value='Python'>Python</option>" +
+                "</select>" + "<br/>" +
+                "<input type='submit' value='Submit'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
     }
 
-    @PostMapping("/form")
+    /*@PostMapping("/form")
     public String processForm(@RequestParam String name, @RequestParam String first,
                               @RequestParam String second, @RequestParam String third){
         return "<html>" +
                 "<body>" +
-                "<h1></h1>" +
+                "<h1>"+ name + "</h1>" +
+                "<ol>" +
+                "<li>" + first + "</li>" +
+                "<li>" + second + "</li>" +
+                "<li>" + third + "</li>" +
+                "</ol>" +
+                "<p>Click this <a href='/form'>form</a> to redo.</p>" +
+                "<p>Click <a href='/'>here</a> to return to the home page.</p>" +
+                "</body>" +
+                "</html>";
+    }*/
+
+    /*@PostMapping("/form")
+    public String processForm(@RequestParam String name, @RequestParam String first,
+                              @RequestParam String second, @RequestParam String third){
+        return "<html>" +
+                "<body>" +
+                "<h1>"+ name + "</h1>" +
+                "<table>" +
+                "<tr>" +
+                "<th>Preference</th>" +
+                "<th>Language</th>" +
+                "</tr>" +
+                "<tr>" +
+                "<td>1</td>" +
+                "<td>" + first + "</td>" +
+                "</tr>" +
+                "<tr>" +
+                "<td>2</td>" +
+                "<td>" + second + "</td>" +
+                "</tr>" +
+                "<tr>" +
+                "<td>3</td>" +
+                "<td>" + third + "</td>" +
+                "</tr>" +
+                "</table>" +
+                "<p>Click this <a href='/form'>form</a> to redo.</p>" +
+                "<p>Click <a href='/'>here</a> to return to the home page.</p>" +
+                "</body>" +
+                "</html>";
+    }*/
+
+    @GetMapping("completed")
+    public String processForm(@RequestParam String name, @RequestParam String first,
+                              @RequestParam String second, @RequestParam String third){
+        return "<html>" +
+                "<body>" +
+                "<h1>"+ name + "</h1>" +
+                "<table>" +
+                "<tr>" +
+                "<th>Preference</th>" +
+                "<th>Language</th>" +
+                "</tr>" +
+                "<tr>" +
+                "<td>1</td>" +
+                "<td>" + first + "</td>" +
+                "</tr>" +
+                "<tr>" +
+                "<td>2</td>" +
+                "<td>" + second + "</td>" +
+                "</tr>" +
+                "<tr>" +
+                "<td>3</td>" +
+                "<td>" + third + "</td>" +
+                "</tr>" +
+                "</table>" +
+                "<p>Click <a href='/form'>here</a> to fill out form again.</p>" +
+                "<p>Click <a href='/'>here</a> to return to the home page.</p>" +
                 "</body>" +
                 "</html>";
     }
